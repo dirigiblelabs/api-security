c90f2e6d21106b70b9269b2578a92423a1eb95a4
Nedelcho Delchev <delchevn@gmail.com>
2021-07-23 09:38:47 +0300

    Update user.js

--- security/v4/user.js
+++ security/v4/user.js
@@ -1,17 +1,17 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * Contributors:
- *   SAP - initial API and implementation
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
  */
-
 /**
  * API v4 User
- * 
+ *
  * Note: This module is supported only with the Mozilla Rhino engine
  */
 
@@ -21,4 +21,24 @@ exports.getName = function() {
 
 exports.isInRole = function(role) {
 	return org.eclipse.dirigible.api.v3.security.UserFacade.isInRole(role);
+};
+
+exports.getTimeout = function() {
+	return org.eclipse.dirigible.api.v3.security.UserFacade.getTimeout();
+};
+
+exports.getAuthType = function() {
+	return org.eclipse.dirigible.api.v3.security.UserFacade.getAuthType();
+};
+
+exports.getSecurityToken = function() {
+	return org.eclipse.dirigible.api.v3.security.UserFacade.getSecurityToken();
+};
+
+exports.getInvocationCount = function() {
+	return org.eclipse.dirigible.api.v3.security.UserFacade.getInvocationCount();
+};
+
+exports.getLanguage = function() {
+	return org.eclipse.dirigible.api.v3.security.UserFacade.getLanguage();
 };
