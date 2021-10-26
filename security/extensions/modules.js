54ad1b74162127cda4c3c045a398156969058e98
Ivan <iwolkkov@gmail.com>
2021-10-26 10:11:25 +0300

    Added d.ts for module.

--- security/extensions/modules.js
+++ security/extensions/modules.js
@@ -1,19 +1,16 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * Contributors:
- *   SAP - initial API and implementation
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
  */
-exports.getContent = function() {
-	return [{
-		name: "security/v4/user",
-		description: "User API"
-	}, {
-		name: "security/v4/oauth",
-		description: "OAuth API"
-	}];
+const registry = require('platform/v4/registry');
+exports.getContent = function () {
+    const file = registry.getText("security/extensions/modules.json");
+    return JSON.parse(file);
 };
